66346b90ff9213f5bb54174b33ecfb2f90d9e38d
jomcarvajal <jose.carvajal@edify.cr>
2022-08-09 14:57:56 -0600

    feat(authservice): install angular firebase and create register, login and logout logic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB3D-f2MON4l_Eps2TO5dQ6nFgqUPhNxoQ",
+    authDomain: "clips-8bd16.firebaseapp.com",
+    projectId: "clips-8bd16",
+    storageBucket: "clips-8bd16.appspot.com",
+    appId: "1:897116898457:web:a8463f007554b3e0242408"
+  },
 };
 
 /*
