e153dbfe2a5a4310a584730feb3537118c4b5a86
jomcarvajal <jose.carvajal@edify.cr>
2022-08-16 13:11:41 -0600

    feat(video): add routing logic

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +13,6 @@ export class NavComponent implements OnInit {
   constructor(
     public modal: ModalService,
     public auth: AuthService,  
-    public afAuth: AngularFireAuth,
   ) { 
   }
 
@@ -24,10 +23,4 @@ export class NavComponent implements OnInit {
     event.preventDefault();
     this.modal.toggleModal('auth');
   }
-
-  async logout($event: Event) {
-    $event.preventDefault();
-    await this.afAuth.signOut();
-  }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
-import { map, delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, delay, filter, switchMap } from 'rxjs/operators';
 import IUser from '../models/user.model';
+import { Router } from '@angular/router'; 
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +14,13 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
+  private redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore,
+    private router: Router,
+    private route: ActivatedRoute,
   ) { 
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(
@@ -24,6 +29,13 @@ export class AuthService {
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
       delay(1000)
     );
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.route.firstChild),
+      switchMap(route => route?.data ?? of({}))
+    ).subscribe(data => {
+      this.redirect = data.authOnly ?? false
+    });
   }
 
   public async createUser(userData: IUser) {
@@ -48,5 +60,15 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name,
     })
+  }
+
+  public  async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+    await this.auth.signOut();
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
